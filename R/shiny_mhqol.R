f1c67288031da6079646c2b37a546b19a02dc4ea
stijnbpeeters <s.b.peeters@eshpm.eur.nl>
2025-02-26 12:48:35 +0100

    Changes for CRAN submission

--- R/shiny_mhqol.R
+++ R/shiny_mhqol.R
@@ -8,39 +8,43 @@
 #' @param display.mode The display mode to be passed to \link[shiny]{runApp}. Default is "normal".
 #' @return NULL (launches the Shiny app).
 #' @examples
-#' \dontrun{
-#' shiny_mhqol()
-#' shiny_mhqol(display.mode = "normal")
+#' if(interactive()) {
+#'   shiny_mhqol()
+#'   shiny_mhqol(display.mode = "normal")
 #' }
 #' @export
 
 
 shiny_mhqol <- function(display.mode = "normal") {
-  # List of required packages
-  pkgs <- c(
-    "shiny", "tidyverse", "DT", "writexl", "mhqol",
-    "fmsb", "here", "shinyalert"
-  )
-
-  # Check for missing packages
-  missing <- sapply(pkgs, function(x) !requireNamespace(x, quietly = TRUE))
-
-  # Stop execution if any required packages are missing
-  if (any(missing)) {
-    stop(
-      paste(
-        "The following package(s) are required for shiny_mhqol to work:",
-        paste(pkgs[missing], collapse = ", ")
-      ),
-      call. = FALSE
+  if (interactive()) {
+    # List of required packages
+    pkgs <- c(
+      "shiny", "tidyverse", "DT", "writexl", "mhqol",
+      "fmsb", "here", "shinyalert"
     )
-  }
 
-  # Locate and run the Shiny app
-  app_dir <- system.file("shiny", package = "mhqol")
-  if (app_dir == "") {
-    stop("Shiny app directory not found in the 'mhqol' package.", call. = FALSE)
-  }
+    # Check for missing packages
+    missing <- sapply(pkgs, function(x) !requireNamespace(x, quietly = TRUE))
 
-  shiny::runApp(app_dir, display.mode = display.mode)
+    # Stop execution if any required packages are missing
+    if (any(missing)) {
+      stop(
+        paste(
+          "The following package(s) are required for shiny_mhqol to work:",
+          paste(pkgs[missing], collapse = ", ")
+        ),
+        call. = FALSE
+      )
+    }
+
+    # Locate and run the Shiny app
+    app_dir <- system.file("shiny", package = "mhqol")
+    if (app_dir == "") {
+      stop("Shiny app directory not found in the 'mhqol' package.", call. = FALSE)
+    }
+
+    shiny::runApp(app_dir, display.mode = display.mode)
+  } else {
+    stop("shiny_mhqol() should only be run in an interactive session.", call. = FALSE)
+  }
 }
